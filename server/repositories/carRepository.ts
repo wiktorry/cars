a06392d16b9c40e9dd7de3c238c4e3acfd350874
wiktorry <wiktorszulc4@gmail.com>
2024-07-06 15:06:57 +0200

    feat: add gitignore file

--- server/repositories/carRepository.ts
+++ server/repositories/carRepository.ts
@@ -5,66 +5,46 @@ import { Car } from "../entities/Car";
 const db: Connection = require("../dbConnection");
 
 const findAllCars = async function () {
-  try {
-    const [result] = await db.promise().execute("SELECT * from cars");
-    return result;
-  } catch {
-    throw error;
-  }
+  const [result] = await db.promise().execute("SELECT * from cars");
+  return result;
 };
 
 const findCarById = async function (id: number) {
-  try {
-    const result = await db
-      .promise()
-      .execute(`SELECT * from cars WHERE carId = ${id}`);
-    return result;
-  } catch {
-    throw error;
-  }
+  const result = await db
+    .promise()
+    .execute(`SELECT * from cars WHERE carId = ${id}`);
+  return result;
 };
 
 const createCar = async function (car: Car) {
-  try {
-    const [result]: any = await db
-      .promise()
-      .execute(
-        `INSERT INTO cars (carId, brand, model) VALUES (NULL, "${car.brand}", "${car.model}")`
-      );
-    const newCar = await db
-      .promise()
-      .execute(`SELECT * FROM cars WHERE carId = ${result.insertId}`);
-    return newCar;
-  } catch {
-    throw error;
-  }
+  const [result]: any = await db
+    .promise()
+    .execute(
+      `INSERT INTO cars (carId, brand, model) VALUES (NULL, "${car.brand}", "${car.model}")`
+    );
+  const newCar = await db
+    .promise()
+    .execute(`SELECT * FROM cars WHERE carId = ${result.insertId}`);
+  return newCar;
 };
 
 const updateCar = async function (car: Car) {
-  try {
-    const [result] = await db
-      .promise()
-      .execute(
-        `UPDATE cars SET brand = "${car.brand}", model = "${car.model}" WHERE carId = "${car.id}"`
-      );
-    const updatedCar = await db
-      .promise()
-      .execute(`SELECT * FROM cars WHERE carId = "${car.id}"`);
-    return updatedCar;
-  } catch {
-    throw error;
-  }
+  const [result] = await db
+    .promise()
+    .execute(
+      `UPDATE cars SET brand = "${car.brand}", model = "${car.model}" WHERE carId = "${car.id}"`
+    );
+  const updatedCar = await db
+    .promise()
+    .execute(`SELECT * FROM cars WHERE carId = "${car.id}"`);
+  return updatedCar;
 };
 
 const deleteCarById = async function (id: number) {
-  try {
-    const result = await db
-      .promise()
-      .execute(`DELETE from cars WHERE carId = ${id}`);
-    return result;
-  } catch {
-    throw error;
-  }
+  const result = await db
+    .promise()
+    .execute(`DELETE from cars WHERE carId = ${id}`);
+  return result;
 };
 
 module.exports = {
--- server/repositories/userRepository.ts
+++ server/repositories/userRepository.ts
@@ -5,34 +5,26 @@ import { error } from "console";
 const db: Connection = require("../dbConnection");
 
 const createUser = async function (user: User) {
-  try {
-    const [result]: any = await db
-      .promise()
-      .execute(
-        `INSERT INTO users (userId, email, password) VALUES (NULL, "${user.email}", "${user.password}")`
-      );
-    const newUser = await db
-      .promise()
-      .execute(
-        `SELECT userId, email FROM users WHERE userId = ${result.insertId}`
-      );
-    return newUser;
-  } catch {
-    throw error;
-  }
+  const [result]: any = await db
+    .promise()
+    .execute(
+      `INSERT INTO users (userId, email, password) VALUES (NULL, "${user.email}", "${user.password}")`
+    );
+  const newUser = await db
+    .promise()
+    .execute(
+      `SELECT userId, email FROM users WHERE userId = ${result.insertId}`
+    );
+  return newUser;
 };
 
 const findUserByEmail = async function (email: string) {
-  try {
-    const [result] = await db
-      .promise()
-      .execute(
-        `SELECT userId, email, password FROM users WHERE email = "${email}"`
-      );
-    return result;
-  } catch {
-    throw error;
-  }
+  const [result] = await db
+    .promise()
+    .execute(
+      `SELECT userId, email, password FROM users WHERE email = "${email}"`
+    );
+  return result;
 };
 
 module.exports = {
