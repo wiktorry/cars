7736a407b46023f7ce7fd6d0c044bdb3dbf0e9e7
wiktorry <wiktorszulc4@gmail.com>
2024-07-11 13:55:54 +0200

    fix: repository queries

--- server/repositories/userRepository.ts
+++ server/repositories/userRepository.ts
@@ -6,32 +6,33 @@ const db: Connection = require("../dbConnection");
 const createUser = async function (user: User) {
   const [result]: any = await db
     .promise()
-    .execute(
-      `INSERT INTO users (userId, email, password) VALUES (NULL, "${user.email}", "${user.password}")`
-    );
+    .execute(`INSERT INTO users (userId, email, password) VALUES (NULL, ? ?)`, [
+      user.email,
+      user.password,
+    ]);
   const newUser = await db
     .promise()
-    .execute(
-      `SELECT userId, email FROM users WHERE userId = ${result.insertId}`
-    );
+    .execute(`SELECT userId, email FROM users WHERE userId = ?`, [
+      result.insertId,
+    ]);
   return newUser;
 };
 
 const findUserByEmail = async function (email: string) {
   const [result] = await db
     .promise()
-    .execute(
-      `SELECT userId, email, password FROM users WHERE email = "${email}"`
-    );
+    .execute(`SELECT userId, email, password FROM users WHERE email = ?`, [
+      email,
+    ]);
   return result;
 };
 
 const findUserById = async function (id: number) {
   const [result] = await db
     .promise()
-    .execute(
-      `SELECT userId, email, password FROM users WHERE userId = "${id}"`
-    );
+    .execute(`SELECT userId, email, password FROM users WHERE userId = ?`, [
+      id,
+    ]);
   return result;
 };
 
